e3b2cf556f3259a853ddf8e36d2d6bf93c2a4edf
oreki <514677915@qq.com>
2018-07-31 18:45:43 +0800

    Initial commit

---
+++ src/main/java/xin/oreki/JredisDemoApplication.java
@@ -0,0 +1,12 @@
+package xin.oreki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JredisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JredisDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/xin/oreki/JredisDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package xin.oreki;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JredisDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
